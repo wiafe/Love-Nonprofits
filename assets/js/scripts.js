6c081c68f6b58e7671f043ab64846e93a9c9f00e
Randy Wiafe <wiafe23@gmail.com>
2015-05-22 14:36:54 -0600

    respon

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -10,10 +10,10 @@ $( document ).ready(function() {
 
 	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 
-	// $('.enter').click(function() {
-	// 	var userMessage = $('textarea[name=message]').val();
+	$('#send-message').click(function() {
+		var userMessage = $('#message').val();
 
-	// 	$( '<p>' + userMessage + '</p>' ).prependTo('p');
-	// });
+		$( '<p>' + userMessage + '</p>' ).prependTo('.post-content');
+	});
 
 });
