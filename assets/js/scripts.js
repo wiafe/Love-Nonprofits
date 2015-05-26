ef2cc20635ad5cb618bcdf9d3abaa40d3ac3f948
Randy Wiafe <wiafe23@gmail.com>
2015-05-26 17:26:51 -0600

    Message Responsive

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -21,9 +21,8 @@ $( document ).ready(function() {
     });
 
     $('textarea').textcounter({
-    	type: "characer",
     	max: 100,
-    	stopInputAtMaximum: false
+    	stopInputAtMaximum: true
 	});
 
 });
